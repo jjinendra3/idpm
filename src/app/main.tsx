43ea32d4761c0c182347a590cdc681ccbb10c4f0
GarunJad <garunjadaun@gmail.com>
2025-09-09 20:08:59 +0530

    Add main file

---
+++ src/app/main.tsx
@@ -0,0 +1,9 @@
+import ChatApp from "./page";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ChatApp />
+    </main>
+  );
+}
