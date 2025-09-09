4fdbe504311c1a425a61b7990a26592d31ef9a5e
jjinendra3 <jjinendra3@gmail.com>
2025-09-09 23:25:35 +0530

    chore: file deleted

--- src/app/main.tsx
+++
@@ -1,9 +0,0 @@
-import ChatApp from "./page";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <ChatApp />
-    </main>
-  );
-}
